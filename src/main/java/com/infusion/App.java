8d9841e190c981b51765dfa437ed3127aab14350
Timofey Volkov <tvolkov@wiley.com>
2015-12-15 15:44:30 +0300

    one more commit

--- src/main/java/com/infusion/App.java
+++ src/main/java/com/infusion/App.java
@@ -2,14 +2,16 @@ package com.infusion;
 
 import com.infusion.correction.CorrectionProvider;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
-    public static final int DEFAULT_QUEUE_CAPACITY = 10000;
+    public static final int DEFAULT_QUEUE_CAPACITY = 100000;
 
     public static void main( String[] args ) {
+        Date start = new Date();
 
         Map<String, MeanCalculator> meanCalculatorMap = new HashMap<String, MeanCalculator>(){{
             put("INSTRUMENT1", new MeanCalculator());
@@ -23,7 +25,12 @@ public class App {
                 return 1d;
             }
         };
-        new InstrumentMeanValuesCalculationEngine("src/test/resources/example_input.txt",
+        new InstrumentMeanValuesCalculationEngine("src/test/resources/large_file.txt",
             meanCalculatorMap, correctionProvider, DEFAULT_QUEUE_CAPACITY).calculateMetrics();
+
+        Date end = new Date();
+
+        long diff = end.getTime() - start.getTime();
+        System.out.println(diff / 1000);
     }
 }
--- src/test/resources/TestFileGenerator.groovy
+++ src/test/resources/TestFileGenerator.groovy
@@ -1,1 +1,40 @@
-println 'generating large file'
+import java.util.concurrent.ThreadLocalRandom
+
+println 'generating large input file'
+
+File file = new File('src/test/resources/large_file.txt')
+if (file.exists()){
+    println 'file already exists, returning'
+    return
+}
+
+def numberOfLines = 50000000
+for (int i = 0; i < numberOfLines; i++){
+    "${ThreadLocalRandom.current().nextInt(28)}-${getMonthName(ThreadLocalRandom.current().nextInt(12))}-${getRandomYear()}"
+
+    file << "INSTRUMENT${instrumentId},${date},${price}"
+}
+
+def getInstrumentId(){
+    def maxInstrumentId = 10000
+    String.valueOf(ThreadLocalRandom.current().nextInt(maxInstrumentId))
+}
+
+String getDate(){
+    def maxMonth = 12
+    "${ThreadLocalRandom.current().nextInt(28)}-${getMonthName(ThreadLocalRandom.current().nextInt(maxMonth))}-${getRandomYear()}"
+}
+
+def getPrice(){
+    ThreadLocalRandom.current().nextDouble(2.000, 200.000)
+}
+
+def getMonthName(int number){
+    def months = [1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"]
+    months[number]
+}
+
+def getRandomYear(){
+    int minYear = 1996, maxYear = 2014
+    ThreadLocalRandom.current().nextInt(minYear, maxYear + 1)
+}
