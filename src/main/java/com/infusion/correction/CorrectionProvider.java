e1a1f0a833b1993baea633dd953b6e8a0bbff1ef
tvolkov <timofey.volkov@gmail.com>
2015-12-15 22:22:18 +0300

    added comment

--- src/main/java/com/infusion/correction/CorrectionProvider.java
+++ src/main/java/com/infusion/correction/CorrectionProvider.java
@@ -4,5 +4,8 @@ package com.infusion.correction;
  * Created by tvolkov on 12.12.15.
  */
 public interface CorrectionProvider {
+
+    //TODO we will cache the correction data.
+    //TODO Cache will be invalidated either on db trigger or on quartz timer (need to decide)
     double getCorrectionForInstrument(String instrument);
 }
